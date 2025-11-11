aa9b0a72d9218533d575da3e2722d190fa214f3e
Sidsd110401 <sid.deshetti@gmail.com>
2025-11-11 19:55:13 +0530

    first commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
         <div onClick={() => setShowUserLogin(false)} className='fixed top-0 bottom-0 left-0 right-0 z-30 flex items-center text-sm text-gray-600 bg-black/50'>
             <form onSubmit={onSubmitHandler} onClick={(e) => e.stopPropagation()} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
                 <p className="text-2xl font-medium m-auto">
-                    <span>User</span> {state === "login" ? "Login" : "Sign Up"}
+                    <span>User</span> {state === "login" ? "Loginn" : "Sign Up"}
                 </p>
                 {state === "register" && (
                     <div className="w-full">
